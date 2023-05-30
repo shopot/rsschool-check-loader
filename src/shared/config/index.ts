e390ba6bff693893c3e75674f0f2fdac5da14a9c
Sapronov Konstantin <ks.costas@gmail.com>
2023-05-30 08:48:26 +0300

    fix: change json file url

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,4 +1,4 @@
 export const CANCEL_FETCH_TIMEOUT = 5000;
 
 export const TASK_JSON_URL =
-  'https://raw.sgithubusercontent.com/rolling-scopes-school/checklist/master/active-tasks/react_graphiql.json';
+  'https://raw.githubusercontent.com/rolling-scopes-school/checklist/master/active-tasks/react_graphiql.json';
