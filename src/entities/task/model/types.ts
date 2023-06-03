c2d922511901e7ce07df01c272f2f076f6fcdda8
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-03 11:56:21 +0300

    refactor: rename type

--- src/entities/task/model/types.ts
+++ src/entities/task/model/types.ts
@@ -1,6 +1,6 @@
 import { StateCreator } from 'zustand';
 
-export type TypeCriteriaItem = {
+export type TypeCriteria = {
   id: number;
   type: string;
   title?: string;
@@ -13,19 +13,22 @@ export type TypeResponseJSONObject = {
   taskName: string;
   github: string;
   information: string;
-  criteria: Partial<TypeCriteriaItem>[];
+  criteria: Partial<TypeCriteria>[];
 } | null;
 
 export interface ITaskState {
   isLoading: boolean;
   taskName: string;
   taskInformation: string;
-  criteriaResults: TypeCriteriaItem[];
-  totalPoints: number;
+  criteriaResults: TypeCriteria[];
   maxTotalPoints: number;
   error: string;
   fetchTask: (url: string) => void;
   initTask: (data: TypeResponseJSONObject) => void;
+  reset: () => void;
+  resetTotalPoints: () => void;
+  setCriteriaPoints: (id: number, value: number) => void;
+  totalPoints: () => number;
 }
 
 export type TypeTaskStore = StateCreator<ITaskState>;
