90422e6f049f8b76d2fd573e4a3ad6b05264f6b4
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-08 11:42:05 +0300

    feat: add style for total points

--- src/shared/ui/total-points/index.tsx
+++ src/shared/ui/total-points/index.tsx
@@ -1,11 +1,15 @@
 import { Typography } from 'antd';
 
+import styles from './styles.module.scss';
+
 export const TotalPoints = ({ points }: IProps): JSX.Element => {
   const { Title } = Typography;
 
+  const pointsClass = points > 0 ? styles.fill : styles.empty;
+
   return (
     <Title level={4} style={{ textAlign: 'right' }}>
-      Total points {points}
+      <span className={pointsClass}>Total points {points}</span>
     </Title>
   );
 };
