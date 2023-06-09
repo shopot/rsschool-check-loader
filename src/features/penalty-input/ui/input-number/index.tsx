fea114dcae421db2ab8414db3d3597a9ac90902d
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-09 13:47:13 +0300

    fix: remove step equal 0

--- src/features/penalty-input/ui/input-number/index.tsx
+++ src/features/penalty-input/ui/input-number/index.tsx
@@ -3,12 +3,14 @@ import { Form, InputNumber as AntdInputNumber } from 'antd';
 import styles from './styles.module.scss';
 
 export const InputNumber = ({ fieldName, max, onChange }: InputNumberProps): JSX.Element => {
+  const stepValue = Math.abs(max) > 0 ? Math.abs(max) : 1;
+
   return (
     <Form.Item name={fieldName} className={styles.formItem} initialValue={0}>
       <AntdInputNumber
         size="small"
         min={0}
-        step={Math.abs(max)}
+        step={stepValue}
         onChange={(value) => onChange((value || 0) * -1)}
         className={styles.inputNumber}
       />
