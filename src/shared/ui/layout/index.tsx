617d005772ccd091f1d7e27516a54bc6f1c53b8b
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-09 14:57:04 +0300

    feat: add margin

--- src/shared/ui/layout/index.tsx
+++ src/shared/ui/layout/index.tsx
@@ -3,14 +3,18 @@ import { Outlet } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
-export const Layout = ({ slotFooter }: LayoutProps) => (
-  <AntdLayout className={styles.layout}>
-    <AntdLayout.Content className={styles.content}>
-      <Outlet />
-    </AntdLayout.Content>
-    <AntdLayout.Footer className={styles.footer}>{slotFooter}</AntdLayout.Footer>
-  </AntdLayout>
-);
+export const Layout = ({ slotFooter }: LayoutProps) => {
+  const { Content, Footer } = AntdLayout;
+
+  return (
+    <AntdLayout className={styles.layout}>
+      <Content className={styles.content}>
+        <Outlet />
+      </Content>
+      <Footer className={styles.footer}>{slotFooter}</Footer>
+    </AntdLayout>
+  );
+};
 
 interface LayoutProps {
   slotFooter: JSX.Element;
