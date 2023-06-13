a49ecca02d37389942d809a4b179168bec9c5b1b
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-13 12:41:52 +0300

    refactor: rename tasks directory

--- src/shared/config/base.ts
+++ src/shared/config/base.ts
@@ -1,2 +1,5 @@
+// export const CHECKLIST_ACTIVE_TASK_URL =
+//   'https://api.github.com/repos/rolling-scopes-school/checklist/contents/active-tasks';
+
 export const CHECKLIST_ACTIVE_TASK_URL =
-  'https://api.github.com/repos/rolling-scopes-school/checklist/contents/active-tasks';
+  'https://api.github.com/repos/shopot/rsschool-check-loader/active-tasks';
