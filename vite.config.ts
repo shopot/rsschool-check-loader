63bb6bec39db557f7347644fd88f5fe9fedcb438
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-14 09:41:11 +0300

    feat: add manual chunks optimisation

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import autoprefixer from 'autoprefixer';
+import { dependencies } from './package.json';
 
 export const pathResolver = (p: string) => resolve(__dirname, '.', p);
+
+const renderChunks = (deps: Record<string, string>) => {
+  let chunks = {};
+
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom', '@types/compose-function'].includes(key)) {
+      return;
+    }
+
+    chunks[key] = [key];
+  });
+
+  return chunks;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +31,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': pathResolver('./src'),
+    },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
     },
   },
 });
