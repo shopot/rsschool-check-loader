50fc47ccce0b61898e98160c902802cd872ec854
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-14 08:40:10 +0300

    feat: add supports responsive

--- src/shared/ui/total-points/index.tsx
+++ src/shared/ui/total-points/index.tsx
@@ -8,7 +8,7 @@ export const TotalPoints = ({ points }: TotalPointsProps): JSX.Element => {
   const pointsClass = points > 0 ? styles.fill : styles.empty;
 
   return (
-    <Title level={4} style={{ textAlign: 'right' }}>
+    <Title level={4} className={styles.title}>
       <span className={pointsClass}>Total points {points}</span>
     </Title>
   );
